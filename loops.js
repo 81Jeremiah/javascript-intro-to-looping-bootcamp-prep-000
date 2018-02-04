86ceb97afc585bc6fc7437bc17adbf5aa3af5ba0
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-04 01:22:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,12 +19,13 @@ function whileLoop(n){
 function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
+  }
 
     do {
       array = array.slice (1)
     } while (array.length > 0 && maybeTrue());
       
-    }
+    
     return array 
   
 }
